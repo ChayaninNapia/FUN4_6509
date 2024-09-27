4f0a54bb16006cd8ae51a919e227acd68051b9a2
ChayaninNapia <ChayaninNapia@gmail.com>
2024-09-27 21:00:01 +0700

    mode 1,3 finish

--- src/example_description/scripts/robot_server.py
+++ src/example_description/scripts/robot_server.py
@@ -14,13 +14,10 @@ import math
 class RobotServer(Node):
     def __init__(self):
         super().__init__('robot_server')
-        # self.srv = self.create_service(ChangeMode, 'change_mode', self.change_mode_callback)
+        self.srv = self.create_service(ChangeMode, 'change_mode', self.change_mode_callback)
 
         self.target_sub = None  
         self.joint_pub = self.create_publisher(JointState, '/joint_states', 10)
-        
-        #for auto sub
-        # self.target_sub = self.create_subscription(PoseStamped,'/target',self.target_callback,10)
         
         self.set_task_server = self.create_service(SetTaskspace, 'move_to_taskspace', self.handle_move_to_taskspace)
         self.create_timer(0.01, self.sim_loop_move)
@@ -46,61 +43,67 @@ class RobotServer(Node):
         self.current_joint_state = [0.0, 0.0, 0.0]
         self.get_logger().info('Robot Server Node has been started.')
 
-    # def target_callback(self, msg: PoseStamped):
-    #     x = msg.pose.position.x
-    #     y = msg.pose.position.y
-    #     z = msg.pose.position.z
 
-    #     desired_pose = SE3(x, y, z)
-    #     mask = [1, 1, 1, 0, 0, 0]  # Only consider translation, ignore rotation
-    #     solution = self.robot.ikine_LM(desired_pose, mask=mask)
+    def change_mode_callback(self, request, response):
+        mode_name = ["Inverse Pose Kinematics", "Teleoperation", "Auto"]
+        if request.mode in [1, 2, 3]:
+            self.mode = request.mode
+            self.get_logger().info(f'Changing mode to {self.mode} : {mode_name[request.mode - 1]}')
 
-    #     if solution.success:      
-    #         q_ik = solution.q[:3]
-    #         # self.get_logger().info(f"IK Solution found: {q_ik}")
+            if self.mode == 3:
+                # If mode 1 (Inverse Pose Kinematics), start subscribing to /target topic
+                self.start_target_subscription()
+            else:
+                # If not mode 1, stop subscribing to /target topic
+                self.stop_target_subscription()
 
-    #         joint_msg = JointState()
-    #         joint_msg.header.stamp = self.get_clock().now().to_msg()
-    #         joint_msg.name = self.joint_names
-    #         joint_msg.position = q_ik.tolist()
+            response.success = True
+        else:
+            self.get_logger().warn(f'Received invalid mode: {request.mode}')
+            response.success = False
+        return response
+
+    def start_target_subscription(self):
+        """Start subscribing to the /target topic."""
+        if self.target_sub is None:
+            self.target_sub = self.create_subscription(PoseStamped, '/target', self.target_callback, 10)
+            self.get_logger().info('Subscribed to /target topic.')
+
+    def stop_target_subscription(self):
+        """Stop subscribing to the /target topic."""
+        if self.target_sub is not None:
+            self.destroy_subscription(self.target_sub)
+            self.target_sub = None
+            self.get_logger().info('Unsubscribed from /target topic.')
             
-    #         self.q_d = list(joint_msg.position)
-    #         print(self.q_d)
-    #         # self.get_logger().info("Published joint states for joint_1, joint_2, joint_3.")
-    #     else:
-    #         self.get_logger().warn("Inverse Kinematics did not find a solution for the received target.")
+    def target_callback(self, msg: PoseStamped):
+        
+        if self.mode != 3:
+            self.get_logger().warn("Please change to mode 3 !")
+            return 
+        
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
 
-    # def change_mode_callback(self, request, response):
-    #     mode_name = ["Inverse Pose Kinematics", "Teleoperation", "Auto"]
-    #     if request.mode in [1, 2, 3]:
-    #         self.mode = request.mode
-    #         self.get_logger().info(f'Changing mode to {self.mode} : {mode_name[request.mode - 1]}')
+        desired_pose = SE3(x, y, z)
+        mask = [1, 1, 1, 0, 0, 0]  # Only consider translation, ignore rotation
+        solution = self.robot.ikine_LM(desired_pose, mask=mask)
 
-    #         if self.mode == 1:
-    #             # If mode 1 (Inverse Pose Kinematics), start subscribing to /target topic
-    #             self.start_target_subscription()
-    #         else:
-    #             # If not mode 1, stop subscribing to /target topic
-    #             self.stop_target_subscription()
+        if solution.success:      
+            q_ik = solution.q[:3]
+            # self.get_logger().info(f"IK Solution found: {q_ik}")
 
-    #         response.success = True
-    #     else:
-    #         self.get_logger().warn(f'Received invalid mode: {request.mode}')
-    #         response.success = False
-    #     return response
-
-    # def start_target_subscription(self):
-    #     """Start subscribing to the /target topic."""
-    #     if self.target_sub is None:
-    #         self.target_sub = self.create_subscription(PoseStamped, '/target', self.target_callback, 10)
-    #         self.get_logger().info('Subscribed to /target topic.')
-
-    # def stop_target_subscription(self):
-    #     """Stop subscribing to the /target topic."""
-    #     if self.target_sub is not None:
-    #         self.destroy_subscription(self.target_sub)
-    #         self.target_sub = None
-    #         self.get_logger().info('Unsubscribed from /target topic.')
+            joint_msg = JointState()
+            joint_msg.header.stamp = self.get_clock().now().to_msg()
+            joint_msg.name = self.joint_names
+            joint_msg.position = q_ik.tolist()
+            
+            self.q_d = list(joint_msg.position)
+            print(self.q_d)
+            # self.get_logger().info("Published joint states for joint_1, joint_2, joint_3.")
+        else:
+            self.get_logger().warn("Inverse Kinematics did not find a solution for the received target.")
 
     def sim_loop_move(self):
         msg = JointState()
@@ -123,27 +126,26 @@ class RobotServer(Node):
         self.joint_pub.publish(msg)
         
     def handle_move_to_taskspace(self, request, response):
+        
+        if self.mode != 1:
+            self.get_logger().warn("Please change to mode 1 before using this service!")
+            response.success = False
+            return response
+        
         self.get_logger().info(f"Received target taskspace coordinates: x={request.x}, y={request.y}, z={request.z}")
-
-        # Extract the target task space coordinates (x, y, z) from the request
         x = request.x
         y = request.y
         z = request.z
 
-        # Convert target pose to SE3 format for IK calculation
         desired_pose = SE3(x, y, z)
-        mask = [1, 1, 1, 0, 0, 0]  # Only consider translation, ignore rotation
-
-        # Solve inverse kinematics to find joint positions for the desired pose
+        mask = [1, 1, 1, 0, 0, 0]  
+ 
         solution = self.robot.ikine_LM(desired_pose, mask=mask)
 
         if solution.success:
             q_ik = solution.q[:3]
             self.get_logger().info(f"IK Solution found: {solution.q[:3]}")
             self.q_d = q_ik.tolist()
-            # Simulate moving the robot to the calculated joint positions
-            # (In real implementation, you would send joint commands to the robot)
-            # For now, we assume the robot moves successfully.
 
             response.success = True
             self.get_logger().info("Robot successfully moved to the target task space coordinates.")
